464bf850e02a4478721e8dec5806ed7f2a43ed85
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-05-31 01:08:57 +0100

    Exercício de Variáveis e Operações Matemáticas

---
+++ Exercícios2_10793_02L.py
@@ -0,0 +1,16 @@
+# Exercício de Variáveis e Operações Matemáticas
+# As duas primeiras linhas pedem que o utilizador introduza os números
+print("\n")
+Primeiro_numero = input("Introduza um número inteiro, pf: ")
+print("\n")
+Segundo_numero = input("Introduza outro número inteiro, pf: ")
+print("\n")
+# Estas linhas dão o resultado das operações integradas numa frase explicativa
+print("A soma dos dois números dá " + str(int(Primeiro_numero)+int(Segundo_numero)))
+print("\n")
+print("A subtração do primeiro número pelo segundo dá " + str(int(Primeiro_numero)-int(Segundo_numero)))
+print("\n")
+print("A multiplicação dos dois números dá " + str(int(Primeiro_numero)*int(Segundo_numero)))
+print("\n")
+print("A divisão do primeiro número pelo segundo dá " + str(int(Primeiro_numero)/int(Segundo_numero)))
+print("\n")
