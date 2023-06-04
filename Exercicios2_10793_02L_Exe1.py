a6b030d566e6142da84b51e37adbe1cd24aa51da
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-06-04 19:50:54 +0100

    Vars e Ops Matematicas

---
+++ Exercicios2_10793_02L_Exe1.py
@@ -0,0 +1,18 @@
+# Exercício de Variáveis e Operações Matemáticas
+# As duas primeiras linhas pedem que o utilizador introduza os números
+print("\n")
+Primeiro_numero = input("Introduza um número inteiro, pf: ")
+print("\n")
+Segundo_numero = input("Introduza outro número inteiro, pf: ")
+print("\n")
+print("Obrigado pelo seu input. Aqui estão os seus resultados:")
+print("\n")
+# Estas linhas dão o resultado das operações integradas numa frase explicativa
+print("    SOMA: A soma dos dois números dá " + str(int(Primeiro_numero)+int(Segundo_numero)))
+print("\n")
+print("    SUBTRAÇÃO: A subtração do primeiro número pelo segundo dá " + str(int(Primeiro_numero)-int(Segundo_numero)))
+print("\n")
+print("    MULTIPLICAÇÃO: A multiplicação dos dois números dá " + str(int(Primeiro_numero)*int(Segundo_numero)))
+print("\n")
+print("    DIVISÃO: A divisão do primeiro número pelo segundo dá " + str(int(Primeiro_numero)/int(Segundo_numero)))
+print("\n")
