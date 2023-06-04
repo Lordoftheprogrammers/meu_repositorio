ed1d0c1b2aa0bd4a830c380ffaee5985f76ea744
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-06-04 17:23:29 +0100

    Exercicio de dividir e inverter strings

---
+++ Funções.py
@@ -0,0 +1,9 @@
+# Funções
+# input >> Output
+# Criar uma função que pega num número e que lhe soma 2
+def somar2(numero):
+    return numero+2
+teste=somar2(int(input("Introduza o número: ")))
+print("Esse número mais 2 dá "+str(teste))
+
+# from funcao import juntar_texto (se quisermos ter noutro ficheiro, mas tem de estar na mesma pasta)
