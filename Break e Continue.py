534ee98da431c1c09e18a13fda58f3390937d5c8
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-06-05 21:52:25 +0100

    Segundo passo com While

--- Break e Continue.py
+++ Break e Continue.py
@@ -5,5 +5,13 @@ for i in range(10):
     print(i)
     if i==4:
             break
-    
-    
+
+print("\n")
+
+# Ciclo While
+i=0
+while True:
+    print(i)
+    i = i + 1
+    if i==4:
+        break
