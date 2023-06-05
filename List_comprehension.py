efd5199355b961012a121aaea2d86565c288f531
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-06-05 21:13:53 +0100

    Commit do exercicio na aula

--- List_comprehension.py
+++ List_comprehension.py
@@ -1,6 +1,12 @@
 # List Comprehension
 # Fazer assim só no Python
-lista = [i for i in range(3)]
+""" lista = [i for i in range(3)]
 print(lista)
 lista = ["a"*i for i in range(3)]
-print(lista)
+print(lista) """
+
+Lista_de_numeros_pares = [2*i for i in range(1,11)]
+Lista_de_numeros_pares.append(int(2*9.5))
+Lista_de_numeros_pares.sort()
+Lista_de_numeros_pares.insert(3,5)
+print(Lista_de_numeros_pares)
