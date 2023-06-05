e15d13805c2ba7ca25d5284b3cf1d58ebb1444d8
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-06-05 21:14:44 +0100

    Última alteração

--- List_comprehension.py
+++ List_comprehension.py
@@ -7,6 +7,6 @@ print(lista) """
 
 Lista_de_numeros_pares = [2*i for i in range(1,11)]
 Lista_de_numeros_pares.append(int(2*9.5))
-Lista_de_numeros_pares.sort()
-Lista_de_numeros_pares.insert(3,5)
+#Lista_de_numeros_pares.sort()
+#Lista_de_numeros_pares.insert(3,5)
 print(Lista_de_numeros_pares)
