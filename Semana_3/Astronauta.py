e31950b79d7956c247cf7f618dfbd9be2b7fc246
Lordoftheprogrammers <134558568+Lordoftheprogrammers@users.noreply.github.com>
2023-06-08 18:46:20 +0100

    Comentários e pulls
    Fazer alguns comentários para memória futura e treinar pulls

--- Semana_3/Astronauta.py
+++ Semana_3/Astronauta.py
@@ -1,3 +1,4 @@
+# Criar uma história tão inesperada quanto possível.
 import random
 lista_de_eventos = ["tive um encontro com alienígenas","houve muitas falhas de equipamentos", "fizemos algumas descobertas científicas"]
 lista_de_separadores = ["mas", "porém", "ainda"]
@@ -8,4 +9,5 @@ for i in range(3):
         historia = historia+", "
     historia = historia + lista_de_separadores[random.randint(0,2)] +" "+ lista_de_eventos[random.randint(0,2)]
 historia = historia+"."
+# Ainda não estou muito satisfeito com o resultado porque a história ainda é repetitiva.
 print(historia)
