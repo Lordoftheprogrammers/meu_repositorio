e6a06ea93826fc709109692d767bd4e09a9f5922
Lordoftheprogrammers <134558568+Lordoftheprogrammers@users.noreply.github.com>
2023-06-08 19:42:12 +0100

    Comentar e pulls
    Comentar Atividade_7.py e continuar os pulls

--- Semana_3/Atividade_7.py
+++ Semana_3/Atividade_7.py
@@ -1,5 +1,6 @@
 contador=0
 for i in range(1,100):
+    # Colocar uma condição para verificar se i, ao ser dividido por 3, dá resto zero. Usamos o operador "%" para obter esse resto.
     if i%3==0:
         continue
     else:
