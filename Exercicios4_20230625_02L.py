b5dee6a9376ae3b121f160d07af88c9d4dfbe7e5
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-06-25 18:22:47 +0100

    Terceira versão do Exercício

--- Exercicios4_20230625_02L.py
+++ Exercicios4_20230625_02L.py
@@ -19,4 +19,4 @@ consumo_2000 = data_portugal['2000 [YR2000]'].values[0]
 
 # Calcular e imprimir a variação percentual
 variacao_percentual = calcular_variacao_percentual(consumo_1990, consumo_2000)
-print(str(round(variacao_percentual,2))+"%")
+print("A variação de consumo de energia per capita (kWh), em Portugal, entre 1990 e 2000, foi de " + str(round(variacao_percentual,2))+"%. Um aumento muito significativo.")
