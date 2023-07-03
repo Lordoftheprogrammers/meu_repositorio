549a5ecb0a0774929c56a6640c26acbfc203ccc1
Lordoftheprogrammers <andre.carreiro@gmail.com>
2023-07-03 18:40:45 +0100

    Refatorizado e comentado

--- Exercicios5_20230702_02L.py
+++ Exercicios5_20230702_02L.py
@@ -20,7 +20,6 @@ comentários claros que expliquem o funcionamento do código.
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 # Carregar dados do Excel
 data = pd.read_csv('AtividadePedagogica4_10793_02.csv')
 # Para simplificar, coloquei o ficheiro excel na mesma pasta que o código
@@ -32,51 +31,77 @@ data = data[(data['Coluna_1'].notnull())&(data['Coluna_1']!='..')]
 data = data.reset_index(drop=True) 
 """
 
-# 2. Agrupar os valores por categoria
+# 2. Agrupar os valores por produto, somando a quantidade vendida, reindexando a frame.
 
 data = data.groupby('produto')['quantidade_vendida'].sum()
 data = data.reset_index()
 
-print("Agregada: ",data)
-
 # 3. Ordenar dados (1 vez pelo default do Python, outra pelo Bubble Sort)
 
-# Ordenar pelo deafult do Python
-data_ordenada_qtd_1=data.sort_values('quantidade_vendida')
-print("Sort pré-definido no Python:", data_ordenada_qtd_1)
-# Ordenar pelo Bubble sort
+# Ordenar com o Bubble sort
 
-data_ordenada_qtd_2=data.copy()
+def bubble_sort(dataframe, coluna):
+    
+    """# A Função bubble_sort:
+       ## 1. Recebe dois parâmetros:
+       ### - Uma dataframe: basta por o nome da variável quando invocada
+       ### - Uma coluna: deve ser o nome da coluna entre plicas
+       ## 2. Compara pares de elementos.
+       ## 3. Caso o elemento seguinte seja menor, troca os valores da linha de um pela do outro.
+       ## 4. Assim, sucessivamente, até ter colocado os valores em ordem ascendente"""
+        
+    for linha in range(len(dataframe)):
+        for linha_2 in range(len(dataframe)-linha-1):
+            if dataframe.iloc[linha_2][coluna] > dataframe.iloc[linha_2+1][coluna]:
+                temp = dataframe.iloc[linha_2]
+                dataframe.iloc[linha_2] = dataframe.loc[linha_2+1]
+                dataframe.loc[linha_2+1] = temp
+    return dataframe
+            
+# Ordenar com o Insertion Sort
 
-for linha in range(len(data_ordenada_qtd_2)):
-    print("linha: ", linha)
-    for linha_2 in range(len(data_ordenada_qtd_2)-linha-1):
-        print("linha_2: ", linha_2)
-        if data_ordenada_qtd_2.iloc[linha_2]['quantidade_vendida'] > data_ordenada_qtd_2.iloc[linha_2+1]['quantidade_vendida']:
-            print(data_ordenada_qtd_2)
-            _ = data_ordenada_qtd_2.iloc[linha_2].copy()
-            data_ordenada_qtd_2.iloc[linha_2] = data_ordenada_qtd_2.loc[linha_2+1]
-            data_ordenada_qtd_2.loc[linha_2+1] = _
-print(data_ordenada_qtd_2)
+def insertion_sort(dataframe, coluna):
 
-# Gráfico de Barras da 1
+    """# A Função insertion_sort:
+       ## 1. Recebe dois parâmetros:
+       ### - Uma dataframe: basta por o nome da variável quando invocada
+       ### - Uma coluna: deve ser o nome da coluna entre plicas
+       ## 2. Segmenta a dataframe inicial.
+       ## 3. Compara os elementos dentro de cada segmento.
+       ## 4. Assim, sucessivamente, até ter colocado os valores em ordem ascendente"""
+       
+    for i in range(1, len(dataframe)):
+        temp = dataframe.iloc[i][coluna]
+        j = i - 1
+        while j >= 0 and dataframe.iloc[j][coluna] > temp:
+            dataframe.iloc[j + 1] = dataframe.iloc[j]
+            j -= 1
+        dataframe.iloc[j + 1][coluna] = temp
+    return dataframe
 
-categorias = data_ordenada_qtd_1['produto']
-valores = data_ordenada_qtd_1['quantidade_vendida']
+# Gráfico de Barras para dataframes
 
-plt.bar(categorias, valores)
-plt.xlabel("Categorias")
-plt.ylabel("Valores")
-plt.title("Gráfico de Barras")
-plt.show()
+def Mostrar_df_em_grafico(titulo, dataframe, categoria, valor):
 
-# Gráfico de Barras da 2
+    """# A Função Mostrar_df_em_grafico:
+       ## 1. Recebe quatro parâmetros:
+       ### - Um título, para a janela do gráfico
+       ### - Uma dataframe: basta por o nome da variável quando invocada
+       ### - Uma categoria: o nome da coluna entre plicas, para servir de legenda ao gráfico
+       ### - Valores: o nome da coluna entre plicas, para determinar a altura das barras
+       ## 2. Esta função funciona como um procedimento, executando uma ação, sem devolver qualquer valor."""
 
-categorias = data_ordenada_qtd_2['produto']
-valores = data_ordenada_qtd_2['quantidade_vendida']
+    categorias = dataframe[categoria]
+    valores = dataframe[valor]
 
-plt.bar(categorias, valores)
-plt.xlabel("Categorias")
-plt.ylabel("Valores")
-plt.title("Gráfico de Barras")
-plt.show()
+    plt.bar(categorias, valores)
+    plt.xlabel("Categorias")
+    plt.ylabel("Valores")
+    plt.title(titulo)
+    plt.show()
+
+# Ordenar de várias maneiras, mostrando uma mensagem e um gráfico para cada uma
+
+Mostrar_df_em_grafico("Timsort", data.sort_values('quantidade_vendida'),'produto','quantidade_vendida')
+Mostrar_df_em_grafico("Bubble sort", bubble_sort(data,'quantidade_vendida'),'produto','quantidade_vendida')
+Mostrar_df_em_grafico("Insertion sort", insertion_sort(data,'quantidade_vendida'),'produto','quantidade_vendida')
